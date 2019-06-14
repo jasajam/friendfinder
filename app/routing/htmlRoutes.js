9aa479777f2296575ed96d79cbaa8c6dd4c85de8
JAM <27083244+jasajam@users.noreply.github.com>
2019-06-13 20:59:26 -0700

    initial commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,5 @@
+// 2 routes: get route to /survey going to survey.html
+// / which leads to home.html
+
+
+// for help see hot restaurant - server.js + the routes there
---
+++ server.js
@@ -0,0 +1,22 @@
+// step 1 - require express and path
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("app listening on PORT " + PORT);
+});
