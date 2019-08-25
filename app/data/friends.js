87497822212f12e4abf91955b586b633554f7c38
JAM <27083244+jasajam@users.noreply.github.com>
2019-08-25 15:25:37 -0700

    completed main functionality of app and added explanatory comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,33 @@
-// save data on each user here
-// array of objects
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-
 var friendsArr = [
   {
     id: 0,
     name: "Bob",
-    photo: "https://unsplash.com/photos/uqIAw02CI2g",
+    photo: "https://picsum.photos/id/22/300",
     scores: [3, 5, 2, 1, 5, 4, 1, 4, 3, 2]
   },
   {
     id: 1,
     name: "Juventas",
-    photo: "",
+    photo: "https://picsum.photos/id/1005/300",
     scores: [5, 3, 4, 3, 1, 2, 4, 2, 5, 1]
   },
-  { id: 2, name: "Renato", photo: "", scores: [1, 4, 5, 3, 2, 1, 2, 2, 3, 1] },
+  {
+    id: 2,
+    name: "Renato",
+    photo: "https://picsum.photos/id/1001/200",
+    scores: [1, 4, 5, 3, 2, 1, 2, 2, 3, 1]
+  },
   {
     id: 3,
     name: "Brunhilde",
-    photo: "",
+    photo: "https://picsum.photos/id/1027/300",
     scores: [4, 4, 4, 3, 2, 5, 3, 3, 4, 1]
   },
   { id: 4, name: "Maggie", photo: "", scores: [2, 5, 2, 4, 4, 1, 4, 2, 3, 5] },
   {
     id: 5,
     name: "Mohammed",
-    photo: "",
+    photo: "https://picsum.photos/id/531/300",
     scores: [3, 2, 2, 5, 4, 2, 4, 4, 4, 1]
   },
   { id: 6, name: "Mathis", photo: "", scores: [3, 4, 2, 4, 1, 3, 2, 5, 5, 4] },
@@ -50,19 +36,33 @@ var friendsArr = [
   {
     id: 9,
     name: "Boniface",
-    photo: "",
+    photo: "https://picsum.photos/id/177/300",
     scores: [2, 2, 5, 2, 2, 3, 4, 5, 3, 2]
   },
   {
     id: 10,
     name: "Earnest",
-    photo: "",
+    photo: "https://picsum.photos/id/1083/300",
     scores: [3, 2, 4, 3, 2, 3, 1, 1, 3, 4]
   },
-  { id: 11, name: "Blaine", photo: "", scores: [3, 3, 1, 2, 2, 1, 5, 2, 3, 3] }
+  {
+    id: 11,
+    name: "Blaine",
+    photo: "https://picsum.photos/id/64/300",
+    scores: [3, 3, 1, 2, 2, 1, 5, 2, 3, 3]
+  },
+  {
+    id: 12,
+    name: "Dustin",
+    photo: "https://picsum.photos/id/685/300",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    id: 13,
+    name: "Kavya",
+    photo: "https://picsum.photos/id/656/300",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  }
 ];
 
 module.exports = friendsArr;
-
-// see hot restaurant data file
-// similar - make - make a dummy for testing and then export so available to other files
